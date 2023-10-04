2c26fefce0175781afdef9b7122d97ef2991c50c
Bruno Martins <bgcngm@gmail.com>
2023-10-04 10:15:18 +0100

    ne-common: Skip keymaster vendor deps unless target has TEE
    Change-Id: I23bd70cc1c10f509e259d9c624702c0f1d1c4725

--- ne.mk
+++ ne.mk
@@ -108,6 +108,7 @@ PRODUCT_PACKAGES += \
     optee-module
 
 ## Keymaster
+ifeq ($(TARGET_HAS_TEE),true)
 PRODUCT_PACKAGES += \
     android.hardware.security.keymint-V1-ndk_platform.vendor \
     android.hardware.security.secureclock-V1-ndk_platform.vendor \
@@ -121,8 +122,7 @@ PRODUCT_PACKAGES += \
     libshmemcompat.vendor \
     libsoft_attestation_cert.vendor \
     libtrusty.vendor
-
-ifeq ($(TARGET_HAS_TEE),false)
+else
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1-service
 endif

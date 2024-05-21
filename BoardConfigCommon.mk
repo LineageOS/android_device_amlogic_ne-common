b55b9f1e4cf4f12cd1d05fec638a106473c3de9b
Nolen Johnson <johnsonnolen@gmail.com>
2024-05-21 12:08:04 -0400

    ne: Rebrand g12-common to ne-common
    * ne means new era device (s4, sc2, t7)
    
    Change-Id: Ice9aca22e78f3e54afc0fc9af335e6189437a6ed

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-COMMON_PATH := device/amlogic/g12-common
+COMMON_PATH := device/amlogic/ne-common
 
 ## BUILD_BROKEN_*
 # Needed for systemcontrol blobs copy-files to recovery via TARGET_RECOVERY_DEVICE_DIRS
@@ -59,7 +59,7 @@ TARGET_SYSTEM_PROP += $(COMMON_PATH)/system.prop
 TARGET_VENDOR_PROP += $(COMMON_PATH)/vendor.prop
 
 ## Recovery
-TARGET_RECOVERY_DEVICE_DIRS += vendor/amlogic/g12-common/proprietary
+TARGET_RECOVERY_DEVICE_DIRS += vendor/amlogic/ne-common/proprietary
 ifneq ($(strip $(TARGET_BOOTDEVICE)),)
   TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/init-files/fstab.$(TARGET_BOOTDEVICE).amlogic
 else
@@ -79,4 +79,4 @@ VENDOR_SECURITY_PATCH := 2024-07-01
 include device/amlogic/common/BoardConfigAmlogic.mk
 
 ## Include the common proprietary BoardConfig makefile
-include vendor/amlogic/g12-common/BoardConfigVendor.mk
+include vendor/amlogic/ne-common/BoardConfigVendor.mk

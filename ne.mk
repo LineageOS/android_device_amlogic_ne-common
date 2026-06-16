ad128d009d248f0122df504bb5b2a4ff9f07ea50
Vitaliy Tomin <highwaystar.ru@gmail.com>
2026-06-16 16:12:34 +0800

    ne: Switch to A/B and build needed partitions
    * Rely on existing odm_ext and oem.
    * Use f2fs for data like modern AMLogic.
    * TODO: Move all possible partitions to EROFS once bringup is done.
    
    Co-authored-by: Nolen Johnson <johnsonnolen@gmail.com>
    Change-Id: I13d735e8ccbe396ae02684199899de85f3f0dad5

--- ne.mk
+++ ne.mk
@@ -3,6 +3,25 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+## A/B
+$(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_vendor=true \
+    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+    FILESYSTEM_TYPE_vendor=ext4 \
+    POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    checkpoint_gc \
+    otapreopt_script
+
 ## Attestation
 PRODUCT_COPY_FILES +=  \
     frameworks/native/data/etc/android.hardware.device_unique_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.device_unique_attestation.xml \
@@ -108,7 +127,6 @@ endif
 
 ## Partitions
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
-$(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 
 ## Platform
 TARGET_AMLOGIC_SOC ?= sc2
@@ -126,10 +144,18 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 ## Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr
+
+# Update engine
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier
+
+PRODUCT_PACKAGES_DEBUG += \
+    update_engine_client
 
 ## USB
 PRODUCT_PACKAGES += \

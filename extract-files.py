2aa04cc3b963e68b8d0de8e4c338e6ecb3a8987c
Nolen Johnson <johnsonnolen@gmail.com>
2026-08-03 16:38:12 -0400

    ne: Patch audio components to load newer NDK library versions
    Change-Id: I04c68e022a8cc7772cd8136026d25c018ad272f0

--- extract-files.py
+++ extract-files.py
@@ -32,9 +32,9 @@ blob_fixups: blob_fixups_user_type = {
     ): blob_fixup()
         .replace_needed('android.hardware.graphics.common-V4-ndk.so', 'android.hardware.graphics.common-V7-ndk.so'),
     'vendor/lib/libamlaudiohal@7.0.so': blob_fixup()
-        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V4-cpp.so'),
+        .replace_needed('android.media.audio.common.types-V2-cpp.so', 'android.media.audio.common.types-V5-cpp.so'),
     'vendor/lib/hw/audio.primary.amlogic.so': blob_fixup()
-        .replace_needed('android.hardware.bluetooth.audio-V3-ndk.so', 'android.hardware.bluetooth.audio-V5-ndk.so')
+        .replace_needed('android.hardware.bluetooth.audio-V3-ndk.so', 'android.hardware.bluetooth.audio-V6-ndk.so')
         .add_needed('libbluetooth_audio_session_aidl_shim.so'),
     (
         'vendor/lib/hw/camera.amlogic.so',
